32d34419a40ac8de8a589ec2f8fd6ca5498abf13
Meschack-Rams01 <isakajoel712@gmail.com>
2025-10-15 22:52:01 +0300

    \Enable WhiteNoise to serve static files and streamline deployment configuration"

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: gunicorn didactia_project.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --timeout 120
-worker: celery -A didactia_project worker --loglevel=info
-release: python manage.py migrate --noinput --settings=didactia_project.settings
--- didactia_project/wsgi.py
+++ didactia_project/wsgi.py
@@ -15,20 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'didactia_project.settings')
 
 application = get_wsgi_application()
 
-# One-time startup hooks: run migrations and collectstatic if not already done
-try:
-    RUN_STARTUP = os.environ.get('RUN_DJANGO_STARTUP_TASKS', '1')
-    STARTUP_FLAG = '/tmp/didactai_startup_done'
-    if RUN_STARTUP == '1' and not os.path.exists(STARTUP_FLAG):
-        from django.core.management import call_command
-        # Run migrations (non-interactive)
-        call_command('migrate', interactive=False, verbosity=1)
-        # Collect static files to STATIC_ROOT
-        call_command('collectstatic', interactive=False, clear=True, verbosity=1)
-        # Create a flag file so we don't repeat on subsequent worker spawns
-        with open(STARTUP_FLAG, 'w') as f:
-            f.write('ok')
-except Exception as _e:
-    # Don't crash app if startup tasks fail; app may still serve basic pages
-    pass
-
